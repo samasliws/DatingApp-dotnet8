5174bce448a367513f3660fa9f30174efdc18704
Samsuddin Asliwala <Samsupatel02@gmail.com>
2025-01-25 02:27:23 +0530

    End of section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,32 +1,35 @@
 using System.Collections;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-//[ApiController]
-//[Route("api/[controller]")] //  /api/users
-public class UsersController(DataContext context) : BaseApiController
-{    
-    [AllowAnonymous]
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-    {
-        var users = await context.Users.ToListAsync();
 
-        return users;
+[Authorize]
+public class UsersController(IUserRepository userRepository) : BaseApiController
+{    
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+    {
+        var users = await userRepository.GetUsersAsync();
+        
+        return Ok(users);
     }
 
-    [HttpGet("{id:int}")]  // /api/users/2
-    public async Task<ActionResult<AppUser>> GetUser(int id)
+    [HttpGet("{username}")]  // /api/users/2
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
-        var user = await context.Users.FindAsync(id);
+        var user = await userRepository.GetMemberAsync(username);
 
         if(user == null) return NotFound();                
         
-        return user;        
+        return user;
     }
 }
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -2,6 +2,7 @@ using System;
 using API.Data;
 using API.Interfaces;
 using API.Services;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions;
@@ -18,6 +19,8 @@ public static class ApplicationServiceExtensions
         });
         services.AddCors();
         services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
         return services;
     }
